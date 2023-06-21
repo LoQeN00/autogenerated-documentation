97248df5e5768955c0b5ca27b0d8123ff297dbe8
kozlowskimateusz <mateusz.kozlowski@nextapps.co>
2023-06-21 09:25:28 +0200

    test

---
+++ src/app.documentation.ts
@@ -0,0 +1,39 @@
+import { applyDecorators } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiParam,
+  ApiProperty,
+  ApiUnauthorizedResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
+
+export const ApiOperationBase = ({
+  summary,
+  descriptionText,
+}: ApiOperationBaseArgs) => {
+  return ApiOperation({
+    summary,
+    description: descriptionText,
+  });
+};
+
+interface ApiOperationBaseArgs {
+  summary: string;
+  descriptionText?: string;
+}
+
+export const AppHelloWorlApi = () => {
+  return applyDecorators(
+    ApiOperationBase({
+      summary: 'Login',
+      descriptionText: 'Logs the user into the system',
+    }),
+    ApiOkResponse({
+      status: 200,
+      description: 'The found record',
+      type: String,
+    }),
+  );
+};
